ada6968d9645cdc27f99f13a56f40d22cd9d785e
Nitin Jain <nitin.j@beautifulyears.com>
2015-10-05 19:23:24 +0530

    adding mobile registration

--- src/main/java/com/beautifulyears/constants/BYConstants.java
+++ src/main/java/com/beautifulyears/constants/BYConstants.java
@@ -9,4 +9,8 @@ public final class BYConstants {
 	public static final String SOCIAL_SIGNON_PLATFORM_FACEBOOK = "facebook";
 	
 	public static final int FORGOT_PASSWORD_CODE_EXPIRY_IN_MIN = 30;
+
+	public static final int REGISTRATION_TYPE_EMAIL = 0;
+	
+	public static final int REGISTRATION_TYPE_PHONE = 1;
 }
