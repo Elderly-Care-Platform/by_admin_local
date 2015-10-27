5291fc5143a5d35aa356d464973d46b50be37a1c
saurav suman <sanspoly@gmail.com>
2015-10-27 14:45:34 +0530

    Admin housing update

--- src/main/webapp/js/components/housing/HousingListController.js
+++ src/main/webapp/js/components/housing/HousingListController.js
@@ -1,21 +1,54 @@
 'use strict';
-adminControllers.controller('HousingListController', [ '$scope', '$routeParams', '$location', '$http', function($scope, $routeParams, $location, $http) {
-	
-	$scope.lists = [];
-	
-	if (localStorage.getItem("ADMIN_USER_ROLE") !== 'SUPER_USER' && localStorage.getItem("ADMIN_USER_ROLE") !== 'EDITOR') {
-		$location.path('/users/login');
-		return;
-	}
-	
-	$http.get('/byadmin/api/v1/housing/list/all'
-	).success(function (data) {
-    	$scope.lists=data.data.content;
-    }).error(function (data, status, headers, config) {    
-    	alert(status);
-    });
 
-} ]);
+(function (angular) {
+    'use strict';
+
+    adminControllers
+            .controller('HousingListController', HousingListController)
+            .factory('HousingList', HousingListFactory);
+    
+    HousingListController.$inject = ['$scope', '$q', '$location', '$resource', 'DTOptionsBuilder', 'HousingList'];
+
+    function HousingListController($scope, $q, $location, $resource, DTOptionsBuilder, HousingList) {
+        var vm = this;     
+        
+        if (localStorage.getItem("ADMIN_USER_ROLE") !== 'SUPER_USER' && localStorage.getItem("ADMIN_USER_ROLE") !== 'EDITOR') {
+    		$location.path('/users/login');
+    		return;
+    	}
+        
+        $scope.filters = {
+				cityFilter:0,
+				dateFilter:0,
+				dateStartRange:new Date(),
+				dateEndRange:new Date(),
+			}
+        
+        vm.dtOptions = DTOptionsBuilder.newOptions()
+            .withPaginationType('full_numbers')
+            .withDisplayLength(10)
+        
+        HousingList.getHousingLists().then(function(HousingLists) {
+          vm.myHousingLists = HousingLists;
+        });
+    }
+    
+  HousingListFactory.$inject = ["$http"];
+  
+  function HousingListFactory($http) {
+   return {
+     getHousingLists: function() {
+       return $http.get('/byadmin/api/v1/housing/list/all').then(function(response) {
+         console.log(response.data.data.content);
+         return response.data.data.content;
+       });
+     }
+   };
+  }
+  
+}(angular));
+
+
 
 
 
