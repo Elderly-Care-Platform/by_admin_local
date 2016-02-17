ba3e289c4b3d70b234eb0cdcb2d80ac76b173170
Aparna <aparna.r@beautifulyears.com>
2016-02-17 12:31:30 +0530

    user search with tags

--- src/main/webapp/js/components/userDetails/UserSearchController.js
+++ src/main/webapp/js/components/userDetails/UserSearchController.js
@@ -1,6 +1,6 @@
 adminControllers.controller('UserSearchController', [ '$scope', '$routeParams',
-		'$location', 'UserSearch',
-		function($scope, $routeParams, $location, UserSearch) {
+		'$location', 'UserSearch', 'UserTag', 
+		function($scope, $routeParams, $location, UserSearch, UserTag) {
 			if (localStorage.getItem("ADMIN_USER_ROLE") != 'SUPER_USER') {
 				return;
 			}
@@ -10,11 +10,31 @@ adminControllers.controller('UserSearchController', [ '$scope', '$routeParams',
 			$scope.filters = {
 				"userId" : "",
 				"email" : "",
-				"userName" : ""
+				"userName" : "",
+				"userTags": []
 			};
 			
 			$scope.searchResults = null;
-			$scope.search = function(){
+
+			UserTag.get(function(res) {
+				$scope.existingTags = res.data;
+			}, function(errorResponse) {
+				if (errorResponse.data && errorResponse.data.error && errorResponse.data.error.errorCode === 3002) {
+					$location.path('/users/login');
+					return;
+				}
+				alert("error fetching tags");
+			});
+
+			$scope.allTags = [ "" ];
+	 		
+	 		$scope.addTag = function() {
+				$scope.allTags.push("");
+			}
+
+			
+
+			$scope.search = function(){				
 				var ret = "/users/search?"
 				if($scope.filters.userId){
 					ret += "userId="+$scope.filters.userId + "&";
@@ -25,6 +45,23 @@ adminControllers.controller('UserSearchController', [ '$scope', '$routeParams',
 				if($scope.filters.userName){
 					ret += "userName="+$scope.filters.userName + "&";
 				}	
+				if($scope.allTags.length > 0){
+					$scope.urlTags = [];
+		 			for(var i = 0; i < $scope.allTags.length; i++){
+		 				if($scope.allTags[i] && $scope.allTags[i].id){
+		 					$scope.urlTags.push($scope.allTags[i].id);
+		 				}
+		 			}
+		 			$scope.passTags = [];
+		 			$.each($scope.urlTags, function(i, el){
+					    if($.inArray(el, $scope.passTags) === -1) $scope.passTags.push(el);
+					});		 			
+					$scope.filters.userTags = $scope.passTags;
+					if($scope.filters.userTags && $scope.filters.userTags.length > 0){
+		 				ret += "userTags="+$scope.filters.userTags + "&";
+		 			}
+				}		
+						
 				$location.url(ret);
 			}
 
@@ -40,10 +77,11 @@ adminControllers.controller('UserSearchController', [ '$scope', '$routeParams',
 				});
 			};
 			
-			if($routeParams.userId || $routeParams.email || $routeParams.userName){
+			if($routeParams.userId || $routeParams.email || $routeParams.userName || $routeParams.userTags){
 				$scope.filters.userId = $routeParams.userId;
 				$scope.filters.email = $routeParams.email;
 				$scope.filters.userName = $routeParams.userName;
+				$scope.filters.userTags = $routeParams.userTags;
 				$scope.searchUsers();
 			}
 
